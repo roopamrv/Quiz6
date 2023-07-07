67eb7769fe0980d21a7600e7387c1f58d21eee6d
Vaibhav Rastogi <rastogigr8@gmail.com>
2023-07-06 19:32:06 -0500

    mycommit2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pyodbc
 
 app = Flask(__name__)
 
-
 # SQL Database connection details
 db_server = 'tcp:roopamdb.database.windows.net'
 db_name = 'demodb'
@@ -14,14 +13,21 @@ db_password = 'Test@123'
 conn_str = f"Driver={{ODBC Driver 18 for SQL Server}};Server={db_server};Database={db_name};UID={db_user};PWD={db_password}"
 conn = pyodbc.connect(conn_str)
 
-# # Create table if it doesn't exist
+# # Create tables if they don't exist
 # with conn.cursor() as cursor:
 #     cursor.execute("""
-#         CREATE TABLE stock (
+#         CREATE TABLE IF NOT EXISTS stock (
 #             id INT IDENTITY(1,1) PRIMARY KEY,
 #             item_name VARCHAR(100) NOT NULL
 #         )
-#     """")
+#     """)
+#     cursor.execute("""
+#         CREATE TABLE IF NOT EXISTS transactions (
+#             id INT IDENTITY(1,1) PRIMARY KEY,
+#             shopper_name VARCHAR(100) NOT NULL,
+#             item_name VARCHAR(100) NOT NULL
+#         )
+#     """)
 #     conn.commit()
 
 # Route for home page
@@ -34,6 +40,7 @@ def index():
 def process():
     # Get command from the form
     command = request.form['command']
+    shopper = request.form['shopper']
 
     # Split the command into parts
     parts = command.split()
@@ -42,9 +49,9 @@ def process():
     if parts[0] == 'P':
         # Put item into inventory
         item = parts[1]
-        print(item)
         with conn.cursor() as cursor:
-            cursor.execute("INSERT INTO stock (item_name) VALUES (?)", (item))
+            cursor.execute("INSERT INTO stock (item_name) VALUES (?)", item)
+            cursor.execute("INSERT INTO transactions (item_name) VALUES (?)", item)
             conn.commit()
     elif parts[0] == 'G':
         # Get item from inventory
@@ -54,12 +61,12 @@ def process():
             result = cursor.fetchone()
             if result:
                 item_id = result[0]
-                cursor.execute("DELETE FROM stock WHERE id = (?)", (item_id))
+                cursor.execute("DELETE FROM stock WHERE id = ?", item_id)
+                cursor.execute("INSERT INTO transactions (shopper_name, item_name) VALUES (?, ?)", shopper, item)
                 conn.commit()
-                return f'[{request.form["shopper"]}] G {item}'
+                return f'[{shopper}] G {item}'
             else:
                 return 'Item not available.'
-            
     elif parts[0] == 'I':
         # Show inventory
         with conn.cursor() as cursor:
@@ -67,10 +74,13 @@ def process():
             result = cursor.fetchall()
             inventory = [row[0] for row in result]
             return ', '.join(inventory)
-        
     elif parts[0] == 'T':
         # Show transactions
-        return 'Not implemented'  # Modify this part to fetch and display transactions from a separate table
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT shopper_name, item_name FROM transactions ORDER BY id")
+            result = cursor.fetchall()
+            transactions = [f'[{row[0]}] G {row[1]}' for row in result]
+            return '\n'.join(transactions)
     else:
         return 'Invalid command.'
 
